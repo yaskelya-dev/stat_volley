4f9d83b830afc379f879090bbc486654287806b6
Simon Yaskelya <yaskelya.dev@gmail.com>
2026-09-25 11:46:22 +0300

    Код для добавления

--- users/models.py
+++ users/models.py
@@ -5,7 +5,12 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     birth_date = models.DateField(null=True, blank=True)
-    invite_code = models.CharField(max_length=64, unique=True, blank=True) # код для добавления в совместные редакторы команды
+    invite_code = models.CharField(
+        max_length=64,
+        unique=True,
+        blank=True,
+        null=True
+    ) # код для добавления в совместные редакторы команды
 
     def generate_unique_invite_code(self):
         while True:
