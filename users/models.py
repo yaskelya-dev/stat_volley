357dd846447412421701a9ba0eb15b2dbf26ba29
Simon Yaskelya <yaskelya.dev@gmail.com>
2026-09-25 11:20:26 +0300

    Добавил код для приглашение в совместное редактирование

--- users/models.py
+++ users/models.py
@@ -1,6 +1,14 @@
+import secrets
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
     birth_date = models.DateField(null=True, blank=True)
+    invite_code = models.CharField(max_length=64, unique=True, blank=True) # код для добавления в совместные редакторы команды
+
+    def generate_unique_invite_code(self):
+        while True:
+            code = secrets.token_urlsafe(32)
+            if not CustomUser.objects.filter(invite_code=code).exists():
+                return code
